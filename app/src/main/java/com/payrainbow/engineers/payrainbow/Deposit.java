100ec149fa4ef98e7c9d58f9384a43f32d2f376d
Amon Nyesigye <amonxnye@gmail.com>
2017-06-02 14:03:45 +0300

    Payments Deposit

--- app/src/main/java/com/payrainbow/engineers/payrainbow/Deposit.java
+++ app/src/main/java/com/payrainbow/engineers/payrainbow/Deposit.java
@@ -1,8 +1,11 @@
 package com.payrainbow.engineers.payrainbow;
 
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -29,6 +32,8 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -52,12 +57,20 @@ import java.util.Map;
 public class Deposit extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "PayRainbow_APP" ;
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    final String Token = preferences.getString("Token", "");
+    FirebaseAuth mAuth;
+    FirebaseUser user = mAuth.getCurrentUser();
+    String amount ="";
+    String phone_number ="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_approve);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -73,7 +86,15 @@ public class Deposit extends AppCompatActivity implements NavigationView.OnNavig
             @Override
             public void onClick(View view) {
               //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                VolleyTest();
+
+               // user.getEmail();
+                amount = "2000";
+                phone_number = "0700110561";
+                Volley_Deposit_step_one(phone_number,amount,user.getEmail()+"_"+user.getUid(),Token);
+                //number
+                //amount
+                //external_ref
+                //sender_ref
             }
         });
     }
@@ -143,7 +164,7 @@ public class Deposit extends AppCompatActivity implements NavigationView.OnNavig
     }
 
 
-  public void VolleyTest(){
+  public void Volley_Deposit_step_one(String var_sender_phone, String var_amount, final String var_external_ref, String var_sender_reference ){
 
 
       try {
@@ -152,10 +173,10 @@ public class Deposit extends AppCompatActivity implements NavigationView.OnNavig
           JSONObject jsonBody = new JSONObject();
           jsonBody.put("action", "initiate_mobile_money_payments");
           jsonBody.put("merchant_code", "337");
-          jsonBody.put("sender_reference", "1000");
-          jsonBody.put("sender_phone", "256700110561");
-          jsonBody.put("amount", "2000");
-          jsonBody.put("external_ref", "0001");
+          jsonBody.put("sender_reference", var_sender_reference);
+          jsonBody.put("sender_phone", var_sender_phone);
+          jsonBody.put("amount", var_amount);
+          jsonBody.put("external_ref", var_external_ref);
 
           final String requestBody = jsonBody.toString();
          // final String requestBodyJson = jsonBody.toString();
@@ -194,6 +215,7 @@ public class Deposit extends AppCompatActivity implements NavigationView.OnNavig
                   }
               }
 
+
               @Override
               protected Response<String> parseNetworkResponse(NetworkResponse response) {
                   String responseString = "";
@@ -227,11 +249,17 @@ public class Deposit extends AppCompatActivity implements NavigationView.OnNavig
 
 
                           obj = new JSONObject(json);
+
                            status = obj.getString("status");
+                          request_id = obj.getString("request_id");
+                          status_code = obj.getString("status_code");
+                          message = obj.getString("message");
                           request_id = obj.getString("request_id");
 
                          // Log.i("Poketi", obj);
                           Log.d(TAG, request_id);
+                          Toast.makeText(Deposit.this, status, Toast.LENGTH_SHORT).show();
+                          Volley_Deposit_step_two(request_id, var_external_ref);
 
                       } catch (JSONException e) {
                           e.printStackTrace();
@@ -252,6 +280,250 @@ public class Deposit extends AppCompatActivity implements NavigationView.OnNavig
           e.printStackTrace();
       }
   }
+
+    public void Volley_Deposit_step_two(String var_request_id, final String var_external_ref){
+
+
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "http://pocketmoney.infosis.uk/Api/";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("action", "initiate_mobile_money_payments_confirmed");
+            jsonBody.put("sender_phone", "256700110561");
+            jsonBody.put("request_id", var_request_id);
+
+            final String requestBody = jsonBody.toString();
+            // final String requestBodyJson = jsonBody.toString();
+
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("Poketi", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Poketi", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> params = new HashMap<String, String>();
+                    params.put("Authorization-Key", "CV8FJVBHM6KXHEHUPYGWJJKFYEEWUQN8CKTAXNGRWRUQN9YAHN3");
+                    return params;
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    String json = "";
+                    String status = "";
+                    String status_code = "";
+                    String tx_status = "";
+                    String tx_reference = "";
+                    String message = "";
+                    String action = "";
+
+
+
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                        try {
+
+                            json = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, json);
+
+                        JSONObject obj = null;
+                        try {
+                            obj = new JSONObject(json);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+
+
+                            obj = new JSONObject(json);
+
+                            status = obj.getString("status");
+                            tx_reference = obj.getString("tx_reference");
+                            tx_status = obj.getString("tx_status");
+                            message = obj.getString("message");
+                            status_code = obj.getString("status_code");
+                            action =  obj.getString("action");
+
+                            // Log.i("Poketi", obj);
+                            Log.d(TAG, tx_reference);
+                            Toast.makeText(Deposit.this, tx_status, Toast.LENGTH_SHORT).show();
+                            Volley_Deposit_step_three(tx_reference,var_external_ref);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                    //return Response.success(request_id);
+
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Volley_Deposit_step_three(String var_tx_reference,String var_external_ref ){
+
+
+
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "http://pocketmoney.infosis.uk/Api/";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("action", "mobile_money_payments_check_status");
+            jsonBody.put("merchant_code", "337");
+            jsonBody.put("tx_reference", var_tx_reference);
+            jsonBody.put("external_ref", var_external_ref);
+
+            final String requestBody = jsonBody.toString();
+            // final String requestBodyJson = jsonBody.toString();
+
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("Poketi", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Poketi", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> params = new HashMap<String, String>();
+                    params.put("Authorization-Key", "CV8FJVBHM6KXHEHUPYGWJJKFYEEWUQN8CKTAXNGRWRUQN9YAHN3");
+                    return params;
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    String json = "";
+                    String status = "";
+                    String status_code = "";
+                    String action = "";
+                    String message = "";
+                    String tx_status = "";
+                    String amount = "";
+                    String payer_phone = "";
+                    String customer_reference = "";
+                    String external_ref = "";
+                    String tx_reference = "";
+
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                        try {
+
+                            json = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, json);
+
+                        JSONObject obj = null;
+                        try {
+                            obj = new JSONObject(json);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+
+
+                            obj = new JSONObject(json);
+                            // Response to Check Transaction Request
+
+                            status = obj.getString("status");
+                            tx_status = obj.getString("tx_status");
+                            amount = obj.getString("amount");
+                            payer_phone = obj.getString("payer_phone");
+                            customer_reference = obj.getString("customer_reference");
+                            tx_reference = obj.getString("tx_reference");
+                            external_ref = obj.getString("external_ref");
+                            status_code = obj.getString("status_code");
+                            message = obj.getString("message");
+
+
+
+                            // Log.i("Poketi", obj);
+                            Log.d(TAG, tx_status);
+
+                            Toast.makeText(Deposit.this, tx_status, Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                    //return Response.success(request_id);
+
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
